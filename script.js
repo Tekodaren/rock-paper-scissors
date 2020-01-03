42101e6e83d442d5870237642c38dd1d73856419
Tekodaren <slaktarne2@gmail.com>
2020-01-03 17:11:38 +0100

    Updated graphics and added a reset button.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var playerScore = 0;
 var computerScore = 0;
+const selector = document.querySelectorAll("#buttons");
+const displayResult = document.querySelector("#matchResult")
+const displayPlayerScore = document.querySelector("#playerScore");
+const displayComputerScore = document.querySelector("#computerScore");
+const displayWinner = document.querySelector("#winner")
+displayPlayerScore.textContent = "You: ";
+displayComputerScore.textContent = "Computer: ";
+
 
 function getRandomInt() {
     let upper = 2;
@@ -14,30 +22,30 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        playerScore++;
+        playerScore += 1;
         return "You win. Rock beats scissors!"
     } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        computerScore++;
+        computerScore += 1;
         return "You lose. Paper beats rock!"
     } else if (playerSelection == "Rock" && computerSelection == "Rock") {
         return "You tied."
     }
     
     if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        playerScore++;
+        playerScore += 1;
         return "You win. Scissors beats paper!"
     } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        computerScore++;
+        computerScore += 1;
         return "You lose. Rock beats scissors!"
     } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
-        return "You tied"
+        return "You tied."
     }
     
     if (playerSelection == "Paper" && computerSelection == "Rock") {
-        playerScore++;
+        playerScore += 1;
         return "You win. Paper beats rock!"
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        computerScore++;
+        computerScore += 1;
         return "You lose. Scissors beats paper!"
     } else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return "You tied."
@@ -46,29 +54,49 @@ function playRound(playerSelection, computerSelection) {
 
 function winner() {
     if (playerScore > computerScore) {
-        text3.textContent = "You win, " + playerScore + " vs " + computerScore + "!";
-      } else if (computerScore > playerScore) {
-        text3.textContent = "You lose, " + playerScore + " vs " + computerScore + "!";
-      } else {
-        text3.textContent = "You tied!";
-      }
+        displayWinner.textContent = "You win, " + playerScore + " vs " + computerScore + "!";
+    } else if (computerScore > playerScore) {
+        displayWinner.textContent = "You lose, " + playerScore + " vs " + computerScore + "!";
+    } else {
+        displayWinner.textContent = "You tied!";
+    }
 }
 
 
+function createResetButton() {
+    const resetButton = document.createElement("button");
+    resetButton.innerText = "RESET";
+    resetButton.style.width = "auto"
+    resetButton.style.height = "auto"
+    resetButton.style.fontSize = "20px"
+    resetButton.style.display = "block";
+    resetButton.style.margin = "auto";
+    resetButton.style.marginTop = "30px";
+    const bottomOfPage = document.querySelector("#winner");
+    bottomOfPage.appendChild(resetButton);
+    resetButton.addEventListener("click", function() {
+        playerScore = 0;
+        computerScore = 0;
+        displayResult.textContent = "";
+        displayPlayerScore.textContent = "You: " + playerScore;
+        displayComputerScore.textContent = "Computer: " + computerScore;
+        displayWinner.textContent = "";
 
-const selector = document.querySelectorAll("#buttons");
+    })
+}
+
 selector.forEach(button => button.addEventListener("click", e => {
-    if (playerScore < 5 && computerScore < 5) {
-        let outcome = playRound(e.srcElement.innerText, computerPlay());
-        displayResult.textContent = outcome;
-        text1.textContent = playerScore;
-        text2.textContent = computerScore;
-    } else {
+    if (playerScore >= 5 || computerScore >= 5) return;                     // Prevents from playing more after game ends.
+    let roundOutcome = playRound(e.srcElement.innerText, computerPlay());
+    displayResult.textContent = roundOutcome;
+    displayPlayerScore.textContent = "You: " + playerScore;
+    displayComputerScore.textContent = "Computer: " + computerScore;
+    if (playerScore == 5 || computerScore == 5) {
         winner();
+        createResetButton();
     }
 }))
 
-const displayResult = document.querySelector("#score")
-const text1 = document.querySelector("#playerScore");
-const text2 = document.querySelector("#computerScore");
-const text3 = document.querySelector("#winner")
+
+
+
